cf8cb69246b1c9fbb4ef5763a48cbef60c471efb
MiklosHorvath <horvathmikloswork@gmail.com>
2022-02-16 17:53:18 +0100

    implement all entities

--- src/main/java/com/codecool/aviationsite/entity/Airplane.java
+++ src/main/java/com/codecool/aviationsite/entity/Airplane.java
@@ -1,4 +1,10 @@
 package com.codecool.aviationsite.entity;
 
 public class Airplane {
+    private String id;
+    private String type;
+    private String baseAirportId;
+    private int fuelInPercent;
+    private int totalFlightHours;
+
 }
--- src/main/java/com/codecool/aviationsite/entity/Airport.java
+++ src/main/java/com/codecool/aviationsite/entity/Airport.java
@@ -1,4 +1,13 @@
 package com.codecool.aviationsite.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Airport {
+    private String id;
+    private String name;
+    private int numberOfAsphaltRunways;
+    private int numberOfGrassRunways;
 }
