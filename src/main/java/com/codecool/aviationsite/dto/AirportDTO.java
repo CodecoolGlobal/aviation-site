85b843754aeaea33c6a43f33b1dce3e74d9307db
MiklosHorvath <horvathmikloswork@gmail.com>
2022-02-16 17:50:23 +0100

    implement all dto

--- src/main/java/com/codecool/aviationsite/dto/AirportDTO.java
+++ src/main/java/com/codecool/aviationsite/dto/AirportDTO.java
@@ -1,4 +1,8 @@
 package com.codecool.aviationsite.dto;
 
 public class AirportDTO {
+    private String type;
+    private String baseAirportId;
+    private int fuelInPercent;
+    private int totalFlightHours;
 }
