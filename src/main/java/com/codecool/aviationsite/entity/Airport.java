001e20e5373ca8b0458c86414245828fd5e4e5ad
MiklosHorvath <horvathmikloswork@gmail.com>
2022-02-16 20:27:51 +0100

    add extra constructor

--- src/main/java/com/codecool/aviationsite/entity/Airport.java
+++ src/main/java/com/codecool/aviationsite/entity/Airport.java
@@ -1,5 +1,6 @@
 package com.codecool.aviationsite.entity;
 
+import com.codecool.aviationsite.dto.AirportDTO;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,4 +11,11 @@ public class Airport {
     private String name;
     private int numberOfAsphaltRunways;
     private int numberOfGrassRunways;
+
+    public Airport(String id, AirportDTO airportDTO){
+        this.id = id;
+        this.name = airportDTO.getName();
+        this.numberOfAsphaltRunways = airportDTO.getNumberOfAsphaltRunways();
+        this.numberOfGrassRunways = airportDTO.getNumberOfGrassRunways();
+    }
 }
